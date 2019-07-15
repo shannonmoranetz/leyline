a5d191d18ae081d02bbc560e7187931ed0b30262
Shannon Moranetz <shannonrmoranetz@gmail.com>
2019-07-15 16:23:47 -0600

    Update assets and reset filename

--- src/containers/Controls.js
+++ src/containers/Controls.js
@@ -56,13 +56,13 @@ class Controls extends Component {
     return (
       <View style={styles.container}>
         <LinearGradient colors={['rgba(245,245,245,.8)', 'rgba(235,235,235,.8)', 'rgba(235,235,235,.8)', 'rgba(245,245,245,.8)'] } locations={[0, .15, 0.85, 1]} style={styles.gradient}>
-        <View style={styles.buttonPositions}>
-          <TouchableOpacity onPress={this.onPressReset}>
-            <Animated.Image source={require('../../assets/reset2.png')} 
-                            style={ !this.props.isConfirmed ? {width: 40, height: 40, transform: [{rotate: spin}]} :
-                            {width: 40, height: 40, transform: [{rotate: spinBack}]} }/>
-          </TouchableOpacity>
-        </View>
+          <View style={styles.buttonPositions}>
+            <TouchableOpacity onPress={this.onPressReset}>
+              <Animated.Image source={require('../../assets/reset.png')} 
+                              style={ !this.props.isConfirmed ? {width: 40, height: 40, transform: [{rotate: spin}]} :
+                              {width: 40, height: 40, transform: [{rotate: spinBack}]} }/>
+            </TouchableOpacity>
+          </View>
         </LinearGradient>
       </View>
     );
